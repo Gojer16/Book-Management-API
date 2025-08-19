ddf1048e290824f9f622cf02a288e526a6d5131c
Gojer <gojer@naver.com>
2025-08-19 10:22:21 -0400

    feat: add setup file with mongodb locally

---
+++ tests/setup.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+beforeAll(async () => {
+  const uri = "mongodb://127.0.0.1:27017/book_api_test";
+  await mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+});
+
+afterEach(async () => {
+  const collections = await mongoose.connection.db.collections();
+  for (let collection of collections) {
+    await collection.deleteMany({});
+  }
+});
+
+afterAll(async () => {
+  await mongoose.connection.dropDatabase(); 
+  await mongoose.connection.close();
+});
