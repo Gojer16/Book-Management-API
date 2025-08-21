20863d9f272b191d1135dcf6908fa692adbaa11d
Gojer <gojer@naver.com>
2025-08-21 18:35:05 -0400

    feat: add cookie-parser

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ app.use(
   })
 );
 
-
+app.use(require('cookie-parser')());
 app.use(express.json({ limit: '1mb' })) 
 app.use(express.urlencoded({ extended: true, limit: '1mb' }));
 app.use(mongoSanitize());
