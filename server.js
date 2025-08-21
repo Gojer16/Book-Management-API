4161eecb1b52b76ec27ce55657b29bcea6f7b3fe
Gojer <gojer@naver.com>
2025-08-21 18:34:22 -0400

    feat: add diff db connection

--- server.js
+++ server.js
@@ -2,11 +2,23 @@ const mongoose = require("mongoose");
 const app = require("./app");
 
 const PORT = process.env.PORT || 5000;
-const DB_URI = process.env.DB_URI || "mongodb://127.0.0.1:27017/book_api";
+let DB_URI;
+
+switch (process.env.NODE_ENV) {
+  case "production":
+    DB_URI = process.env.DB_URI_PROD; 
+    break;
+  case "test":
+    DB_URI = process.env.DB_URI_TEST || "mongodb://127.0.0.1:27017/book_api_test";
+    break;
+  default:
+    DB_URI = process.env.DB_URI_DEV || "mongodb://127.0.0.1:27017/book_api";
+    break;
+}
 
 mongoose.connect(DB_URI)
   .then(() => {
-    console.log("Connected to MongoDB ✅");
+    console.log(`Connected to MongoDB ✅ (${process.env.NODE_ENV})`);
     app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
   })
   .catch((err) => {
