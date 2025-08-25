e7bdfa38c53b19d10e4933e536594a964ed878ec
Gojer <gojer@naver.com>
2025-08-25 17:43:17 -0400

    misc: deleting comments

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,18 +1,17 @@
 const jwt = require('jsonwebtoken');
 
 const authMiddleware = (req, res, next) => {
-  // Get token from header
   const token = req.header('Authorization')?.replace('Bearer ', '');
 
-  // Check if token exists
   if (!token) {
-    return res.status(401).json({ message: 'No token, authorization denied' });
+    return res.status(401).json({ 
+      message: 'No token, authorization denied' 
+    });
   }
 
   try {
-    // Verify token
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded; // Attach user to request object
+    req.user = decoded; 
     next();
   } catch (err) {
     res.status(401).json({ message: 'Token is not valid' });
